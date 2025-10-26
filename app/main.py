b9d7e0a45591e684b9b623e813941504449d7fb0
Yan Tumar <jantumar.unity@gmail.com>
2025-10-26 17:25:37 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -53,21 +53,3 @@ class CarWashStation:
 
         self.average_rating = new_total / self.count_of_ratings
         self.average_rating = round(self.average_rating, 1)
-
-
-bmw = Car(comfort_class=3, clean_mark=3, brand="BMW")
-audi = Car(comfort_class=4, clean_mark=9, brand="Audi")
-
-print(bmw.clean_mark)  # 3
-
-wash_station = CarWashStation(
-    distance_from_city_center=5,
-    clean_power=6,
-    average_rating=3.5,
-    count_of_ratings=6
-)
-
-income = wash_station.serve_cars([bmw, audi])
-
-print(income)  # 6.3
-print(bmw.clean_mark)  # 6
